51d5dacadd7a3d2f2ffbb08c25ee64c9c62804d5
sanjoygoswami973 <sanjoygoswami973@gmail.com>
2023-08-28 16:11:30 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,64 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        fontFamily: {
+            dm: ["DM Sans"],
+            chivo: ["Chivo"],
+        },
+        fontSize: {
+            56: '56px',
+            46: '46px',
+            44: '44px',
+            32: '32px',
+            22: '22px',
+            21: '21px',
+            15: '15px',
+        },
+        colors: {
+            primary: "#3639A4",
+            secondary: "#43E7DF",
+            heading: "#2C2643",
+            paragraph: "#645E76",
+            highlight: "FDB400",
+            third: "#FF7364",
+            four: "#DCE7FE",
+            body: "#F5F5F5"
+        },
+        spacing: {
+            26: "26px",
+            50: "50px",
+            85: "85px",
+            98: "98px",
+            105: "105px",
+            116: "116px",
+            140: "140px",
+            195: "195px",
+            200: "200px",
+            291: "291px",
+            326: "326px",
+        },
+        maxWidth: {
+            container: '1410px',
+            300: "300px",
+            467: "467px",
+            504: "504px",
+            530: "530px",
+            553: "553px",
+            624: "624px",
+            796: "796px",   
+        },
+        minWidth: {
+            300: "300px",
+            480: "480px",
+        },
+        width: {
+            "48%": "48%",
+            "45%": "45%",
+            "32%": "32%",
+        },
+        borderRadius: {
+            '20': '20px',
+        },
+      },
+    },
+  };
